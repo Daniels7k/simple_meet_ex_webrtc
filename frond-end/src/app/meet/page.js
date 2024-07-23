9f0da70056940d315feaca2ec9d6e329e9c039cb
Daniels7k <danielsdev77@gmail.com>
2024-07-23 12:31:14 -0300

    refactor: setting ws behaviour to a func

--- frond-end/src/app/meet/page.js
+++ frond-end/src/app/meet/page.js
@@ -5,18 +5,10 @@ import { useEffect, useState } from "react";
 const PeerPage = () => {
     const [wsConnection, setWsConnection] = useState(null);
 
+
+
     useEffect(() => {
-        const wsConnection = new WebSocket("ws://localhost:4000/ws");
-
-        setWsConnection(wsConnection);
-
-        wsConnection.addEventListener("open", () => {
-            console.log("WebSocket is open now.");
-        });
-
-        wsConnection.addEventListener("message", (event) => {
-            console.log("Message from server: ", event.data);
-        });
+        const wsConnection = startWsConnection();
 
         return () => {
             wsConnection.close();
@@ -31,13 +23,41 @@ const PeerPage = () => {
         }
     }
 
+    const sendAnOffer = () => {
+        if (wsConnection && wsConnection.readyState === WebSocket.OPEN) {
+            wsConnection.send(JSON.stringify({ message: 'Offer from client!' }));
+        } else {
+            console.error("WebSocket is not open.");
+        }
+    }
+
     return (
         <div>
             <p>Hello World!</p>
 
             <button onClick={sendPingToWs}>Send Ping to WS</button>
+            <button onClick={sendAnOffer}>Send Offer to WS</button>
+
         </div>
     );
 };
+
+const startWsConnection = () => {
+    const wsConnection = new WebSocket("ws://localhost:4000/ws");
+
+    wsConnection.addEventListener("open", () => {
+        console.log("WebSocket is open now.");
+    });
+
+    wsConnection.addEventListener("message", (event) => {
+        console.log("Message from server: ", event.data);
+    });
+
+    wsConnection.addEventListener("close", () => {
+        console.log("WebSocket is closed now.");
+    });
+
+    return wsConnection;
+}
 
 export default PeerPage;
